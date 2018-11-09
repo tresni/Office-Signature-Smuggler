f9675e44d6198aebc067114723c3c1d83a91f1ed
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2018-11-09 13:26:00 -0700

    Attempting to randomiz IDs on import

--- sigsmuggle.py
+++ sigsmuggle.py
@@ -18,6 +18,9 @@ import os
 import pickle
 import sqlite3
 import urllib.parse
+import uuid
+
+from random import randint
 
 import click
 
@@ -27,78 +30,98 @@ PATH = '~/Library/Group Containers/UBF8T346G9.Office/Outlook/' \
 
 class Signature(object):
     def __init__(self, recordid, path):
-        profile = click.get_current_context().find_object(Profile)
-        # this is an auto-increment so only used for exporting
-        self.recordid = recordid
-        self.path = self.path = urllib.parse.unquote(path)
-        with open(profile._path(self.path), 'rb') as fp:
+        fpath = urllib.parse.unquote(path)
+        click.echo("Found signature @ %s" % fpath)
+        with open(Profile.getPath(fpath), 'rb') as fp:
             self.contents = fp.read()
         self.OwnedBlocks = []
-        self.__getBlocks()
+        self.__getBlocks(recordid)
 
-    def __getBlocks(self):
-        profile = click.get_current_context().find_object(Profile)
-        cursor = profile.conn.cursor()
+    def __getBlocks(self, recordid):
+        cursor = Profile.getCursor()
         # Not storing the intermediatery Signature_OwnedBlocks table info
         cursor.execute('SELECT b.* FROM Blocks b LEFT JOIN'
                        ' Signatures_OwnedBlocks s ON s.BlockId = b.BlockId'
-                       ' WHERE s.Record_RecordID = ?', (self.recordid,))
+                       ' WHERE s.Record_RecordID = ?', (recordid,))
         for result in cursor:
+            click.echo("Signature owns block %s [%s]" % (result['BlockId'].hex(), result['BlockTag']))
             self.OwnedBlocks.append(Block(*result))
         cursor.close()
 
-    def __str__(self):
-        string = '<Signature %d: %s' % (self.recordid, self.path)
-        if (self.OwnedBlocks):
-            string += ' Blocks: [%s]' % ', '.join(map(str, self.OwnedBlocks))
-        string + '>'
-        return string
-
     def write(self, cursor):
-        profile = click.get_current_context().find_object(Profile)
-        cursor.execute('INSERT OR REPLACE INTO Signatures (Record_RecordID,'
-                       ' PathToDataFile) VALUES  (?, ?)',
-                       (self.recordid, urllib.parse.quote(self.path)))
+        fpath = '/'.join(['Signatures', str(randint(0, 255)), "%s.olk15Signature" %
+                         str.upper(uuid.uuid4().__str__())])
+        click.echo("Writing Signature to %s" % fpath)
+        cursor.execute('INSERT INTO Signatures (PathToDataFile) VALUES  (?)',
+                       (urllib.parse.quote(fpath),))
+        recordid = cursor.lastrowid
         for block in self.OwnedBlocks:
-            cursor.execute('INSERT OR REPLACE INTO Signatures_OwnedBlocks'
+            click.echo("Signature owns block %s [%s]" % (block.id.hex(), block.tag))
+            cursor.execute('INSERT INTO Signatures_OwnedBlocks'
                            ' (Record_RecordID, BlockID, BlockTag) VALUES'
                            ' (?, ?, ?)',
-                           (self.recordid, block.blockid, block.tag))
+                           (recordid, block.id, block.tag))
             block.write(cursor)
-        with open(profile._path(self.path), 'wb') as fp:
+        fpath = Profile.getPath(fpath)
+        os.makedirs(os.path.dirname(fpath), exist_ok=True)
+        with open(fpath, 'wb') as fp:
             fp.write(self.contents)
 
 
 class Block(object):
-    def __init__(self, blockid, tag, path):
-        profile = click.get_current_context().find_object(Profile)
-        self.blockid = blockid
+    def __init__(self, id, tag, path):
+        # This looks to always be b'15000000' but just in case
+        self.header = id[0:4]
         self.tag = tag
-        self.path = urllib.parse.unquote(path)
-        with open(profile._path(self.path), 'rb') as fp:
+        fpath = urllib.parse.unquote(path)
+        click.echo("Block exists at %s" % fpath)
+        with open(Profile.getPath(fpath), 'rb') as fp:
             self.contents = fp.read()
 
-    def __str__(self):
-        return '<Block %s [%s]: %s>' % (self.blockid, self.tag, self.path)
-
     def write(self, cursor):
-        profile = click.get_current_context().find_object(Profile)
-        cursor.execute('INSERT OR REPLACE INTO Blocks (BlockId, BlockTag,'
-                       ' PathToDataFile) VALUES (?, ?, ?)',
-                       (self.blockid, self.tag, urllib.parse.quote(self.path)))
-        with open(profile._path(self.path), 'wb') as fp:
+        fpath = '/'.join(['Signature Attachments', str(randint(0, 255)),
+                          "%s.olk15SigAttachment" %
+                          str.upper(self.uuid.__str__())])
+        click.echo("Writing Block %s to %s" % (self.id.hex(), fpath))
+        cursor.execute('INSERT INTO Blocks (BlockId, BlockTag, PathToDataFile)'
+                       ' VALUES (?, ?, ?)',
+                       (self.id, self.tag, urllib.parse.quote(fpath)))
+        fpath = Profile.getPath(fpath)
+        os.makedirs(os.path.dirname(fpath), exist_ok=True)
+        with open(fpath, 'wb') as fp:
             fp.write(self.contents)
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+        if self.blockid:
+            self.header = self.blockid[0:4]
+            self.uuid = uuid.UUID(bytes=self.blockid[4:])
+        else:
+            self.uuid = uuid.uuid4()
+        self.id = self.header + self.uuid.bytes
 
 
 class Profile(object):
     def __init__(self, profile):
         self.profile = profile
-        self.conn = sqlite3.connect(self._path('Outlook.sqlite'))
+        self.conn = sqlite3.connect(self.__getPath('Outlook.sqlite'))
         self.conn.row_factory = sqlite3.Row
         self.signatures = []
 
-    def _path(self, file):
+    def __getPath(self, file):
         return os.path.expanduser(PATH % self.profile + file)
+
+    @staticmethod
+    def _getProfile():
+        return click.get_current_context().find_object(Profile)
+
+    @staticmethod
+    def getPath(file):
+        return Profile._getProfile().__getPath(file)
+
+    @staticmethod
+    def getCursor():
+        return Profile._getProfile().conn.cursor()
 
     def readSignatures(self):
         cursor = self.conn.cursor()
